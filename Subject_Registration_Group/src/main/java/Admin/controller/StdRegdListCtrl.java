d295c5f46635bba56fee4b46868c82e9842d6ff2
Debasmita-panigrahi <163904440+Debasmita-panigrahi@users.noreply.github.com>
2024-11-15 10:17:18 +0530

    Update StdRegdListCtrl.java

--- Subject_Registration_Group/src/main/java/Admin/controller/StdRegdListCtrl.java
+++ Subject_Registration_Group/src/main/java/Admin/controller/StdRegdListCtrl.java
@@ -19,7 +19,7 @@ public class StdRegdListCtrl extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     
-    public StdRegdListCtrl() 
+    public StdRegdListCtrl() {
         super();
     }
 
